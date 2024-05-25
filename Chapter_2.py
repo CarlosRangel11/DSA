8ea23ead2909100af6466c45f80e1cfb394cebde
Carlos A. Rangel <carange4@asu.edu>
2024-05-25 15:38:54 -0700

    Added timings for bubble, insertion & merge sort

--- Chapter_2.py
+++ Chapter_2.py
@@ -1,4 +1,6 @@
 from time import perf_counter
+from time import sleep
+import numpy as np
 import math
 
 # addressing the sorting problem
@@ -19,21 +21,40 @@ def insertion_sort(array, n):
 
         array[j+1] = key
 
+# Bubble sort
+def bubble_sort(array, n):
+    for i in range(0, n):
+        for j in range(n-1, i, -1):
+            if array[j] < array[j-1]:
+                temp = array[j]
+                array[j] = array[j-1]
+                array[j-1] = temp
+
 
 # Merge sort
+# array: a passed array (duh)
+# p: leftmost index
+# q: mid index
+# r: rightmost index
 def merge(array, p, q, r):
-    left_index = q - p + 1
-    right_index = r - q
 
-    # left = 0:left_index-1 && right = 0:right_index-1
-    left = [None] * left_index-1
-    right = [None] * right_index-1
+    # print(f"Working on subarray with params:\narray: {array}\n\tp = {p}, q = {q}, r = {r}")
+    # sleep(3)
 
-    for i in range(0, left_index-1):
+    left_length = q - p + 1
+    right_length = r - q
+
+    # left = 0:left_length-1 && right = 0:right_length-1
+    left = np.empty(left_length, dtype=int)
+    right = np.empty(right_length, dtype=int)
+
+    # copy left half of array
+    for i in range(0, left_length):
         left[i] = array[p + i]
 
-    for j in range(0, right_index-1):
-        right[j] = array[q + j + 1]
+    # copy right half of array
+    for j in range(0, right_length):
+        right[j] = array[q + 1 + j]
 
     i = 0       # left's smallest remaining element
     j = 0       # right's smallest remaining element
@@ -42,7 +63,7 @@ def merge(array, p, q, r):
     # while each array left, right contains an unmerged element
     # copy the smallest unmerged element back inot array[p:r]
 
-    while i < left_index and j < right_index:
+    while i < left_length and j < right_length:
         if left[i] <= right[j]:
             array[k] = left[i]
             i += 1
@@ -55,13 +76,12 @@ def merge(array, p, q, r):
 
     # after traversing left, right entirely, copy remainder
     # of the other to the end of array[p:r]
-
-    while i < left_index:
+    while i < left_length:
         array[k] = left[i]
         i += 1
         k += 1
 
-    while j < right_index:
+    while j < right_length:
         array[k] = right[j]
         j += 1
         k += 1
@@ -80,14 +100,55 @@ def merge_sort(array, p, r):
 
 
 
-array = [5, 2, 4, 6, 1, 3]
+array = np.random.randint(99, size=(10000))
 
-print(array)
+copy_of_array = array.copy()
+print(f"""
+##############################################################
+Insertion sort on array of length: {len(copy_of_array)}
+""")
+#print(f"{copy_of_array}\n")
+
 
 start_time = perf_counter()
-insertion_sort(array, len(array))
+insertion_sort(copy_of_array, len(copy_of_array))
 end_time = perf_counter()
 
-print("after sorting:")
-print(f"time taken: {end_time - start_time}")
-print(array)
+#print(f"after sorting:\n{copy_of_array}\n")
+
+print(f"time taken: {end_time - start_time : .6f}")
+
+
+
+copy_of_array = array.copy()
+print(f"""
+##############################################################
+Bubble sort on array of length: {len(copy_of_array)}
+""")
+#print(f"{copy_of_array}\n")
+
+
+start_time = perf_counter()
+bubble_sort(copy_of_array, len(copy_of_array))
+end_time = perf_counter()
+
+#print(f"after sorting:\n{copy_of_array}\n")
+
+print(f"time taken: {end_time - start_time : .6f}")
+
+
+
+copy_of_array = array.copy()
+print(f"""
+##############################################################
+Merge sort on array of length: {len(copy_of_array)}
+""")
+#print(f"{copy_of_array}\n")
+
+start_time = perf_counter()
+merge_sort(copy_of_array, 0, len(copy_of_array)-1)
+end_time = perf_counter()
+
+#print(f"after sorting:\n{copy_of_array}\n")
+
+print(f"time taken: {end_time - start_time : .6f}")
