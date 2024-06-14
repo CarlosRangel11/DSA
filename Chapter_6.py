641513e5667a82fae24b2a37b64a517d51bea3af
Carlos Rangel <carange4@asu.edu>
2024-06-13 21:52:58 -0700

    Beginning study of sorting using heaps

---
+++ Chapter_6.py
@@ -0,0 +1,36 @@
+# Max-Heap Property: 
+# Array[parent(i)] >= Array[i]
+
+# Min-Heap Property: 
+# Array[parent(i)] <= Array[i]
+
+class Max_Heap:
+    heapsize = 0
+
+    def __init__(self, heapsize):
+
+
+    def parent(i): 
+        return i // 2
+    
+    def left(i): 
+        return 2 * i
+    
+    def right(i):
+        return 2 * i + 1
+    
+    def max_heapify():
+
+    def build_max_heap():
+
+    def heapsort():
+        
+
+    def insert():
+
+    def extract_max():
+
+    def increase_key():
+
+
+    def peek_max():
