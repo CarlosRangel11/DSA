5a7fa2707549b59bfb358fc448652a996bb0d9f3
Carlos A. Rangel <carange4@asu.edu>
2024-06-17 20:56:02 -0700

    Beginning Heap Studies

--- Chapter_6.py
+++ Chapter_6.py
@@ -1,3 +1,5 @@
+import math
+
 # Max-Heap Property: 
 # Array[parent(i)] >= Array[i]
 
@@ -8,7 +10,7 @@ class Max_Heap:
     heapsize = 0
 
     def __init__(self, heapsize):
-
+        self = [0 for i in range(heapsize)]
 
     def parent(i): 
         return i // 2
@@ -19,11 +21,44 @@ class Max_Heap:
     def right(i):
         return 2 * i + 1
     
-    def max_heapify():
 
-    def build_max_heap():
+    # checks index i of max_heap self to see if it violates the max heap property
+    # runs in O(lg(n))
+    def max_heapify(self, i):
+        left = left(i)
+        right = right(i)
 
-    def heapsort():
+        if left <= self.heapsize and self[left] > self[right]:
+            largest = left
+
+        else: largest = i
+
+        if right <= self.heapsize and self[right] > self[largest]:
+            largest = right
+
+        if largest != i:
+            swap(self, i, largest)
+            self.max_heapify(self, largest)
+            
+
+    # runs max_heapify on half the array, ensuring that the array is sorted. 
+    # O(n), which calls an O(lg(n)) func, so running time = O(nlg(n))
+    def build_max_heap(self, n):
+        self.heapsize = n
+        for i in range((n // 2), 1, -1):
+            self.max_heapify(self, i)
+
+    # goes through the entire array, swapping the top of the heap with the 
+    # botton & runs max_heapify() to compare each node & make sure no child
+    # node is greater than the parent, essentially swapping it.
+    # build_max_heap() := O(n) & max_heapify() := O(lg(n)), therefore
+    # heapsort := O(nlg(n))
+    def heapsort(self, n):
+        self.build_max_heap(self, n)
+        for i in range(n, 2, -1):
+            swap(self, 1, i)
+            self.heapsize = self.heapsize - 1
+            self.max_heapify(self, 1)
         
 
     def insert():
@@ -34,3 +69,10 @@ class Max_Heap:
 
 
     def peek_max():
+
+
+
+def swap(array, x, y):
+    temp = array[x]
+    array[x] = array[y]
+    array[y] = temp
